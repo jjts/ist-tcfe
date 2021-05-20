0a9853e81437021c4f40351c97adc65b0e54ec48
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-05-21 00:55:11 +0100

    add hand calculations and modify octave script accordingly

--- t4/t4.m
+++ t4/t4.m
@@ -25,23 +25,28 @@ gm1=IC1/VT
 rpi1=BFN/gm1
 ro1=VAFN/IC1
 
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+RSB=RB*RS/(RB+RS)
 
-AV1simple = gm1*RC1/(1+gm1*RE1)
+AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+AVI_DB = 20*log10(abs(AV1))
+AV1simple = RB/(RB+RS) * gm1*RC1/(1+gm1*RE1)
+AVIsimple_DB = 20*log10(abs(AV1simple))
 
 RE1=0
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-AV1simple = gm1*RC1/(1+gm1*RE1)
+AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+AVI_DB = 20*log10(abs(AV1))
+AV1simple =  - RSB/RS * gm1*RC1/(1+gm1*RE1)
+AVIsimple_DB = 20*log10(abs(AV1simple))
 
 RE1=100
-
-ZI1 = ((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)
-
-ZX = ro1*((RB+rpi1)*RE1/(RB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RB)+1/RE1+gm1*rpi1/(rpi1+RB)))
-
+ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
+ZX = ro1*((RSB+rpi1)*RE1/(RSB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RSB)+1/RE1+gm1*rpi1/(rpi1+RSB)))
+ZX = ro1*(   1/RE1+1/(rpi1+RSB)+1/ro1+gm1*rpi1/(rpi1+RSB)  )/(   1/RE1+1/(rpi1+RSB) ) 
 ZO1 = 1/(1/ZX+1/RC1)
 
-
+RE1=0
+ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
+ZO1 = 1/(1/ro1+1/RC1)
 
 %ouput stage
 BFP = 227.3
@@ -53,16 +58,19 @@ IE2 = (VCC-VEBON-VI2)/RE2
 IC2 = BFP/(BFP+1)*IE2
 VO2 = VCC - RE2*IE2
 
-
 gm2 = IC2/VT
 go2 = IC2/VAFP
 gpi2 = gm2/BFP
 ge2 = 1/RE2
 
 AV2 = gm2/(gm2+gpi2+go2+ge2)
-
-
-
 ZI2 = (gm2+gpi2+go2+ge2)/gpi2/(gpi2+go2+ge2)
-
 ZO2 = 1/(gm2+gpi2+go2+ge2)
+
+
+%total
+gB = 1/(1/gpi2+ZO1)
+AV = (gB+gm2/gpi2*gB)/(gB+ge2+go2+gm2/gpi2*gB)*AV1
+AV_DB = 20*log10(abs(AV))
+ZI=ZI1
+ZO=1/(go2+gm2/gpi2*gB+ge2+gB)
